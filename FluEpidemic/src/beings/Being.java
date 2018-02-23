6ded5cc32f826aaf78efcf28f8208a3720ec42fa
zhongming-unice <36295024+zhongming-unice@users.noreply.github.com>
2018-02-23 12:11:12 +0100

    first commit

---
+++ FluEpidemic/src/beings/Being.java
@@ -0,0 +1,11 @@
+package beings;
+
+import java.util.*;
+
+/**
+ * @author ZHONG Ming
+ */
+public abstract class Being {
+    private List<State> states = new ArrayList<State>();
+
+}
---
+++ FluEpidemic/src/beings/person/Person.java
@@ -0,0 +1,9 @@
+package beings.person;
+/**
+ * @author ZHONG Ming
+ */
+import beings.Being;
+
+public class Person extends Being{
+    private boolean  hand_washing;
+}
